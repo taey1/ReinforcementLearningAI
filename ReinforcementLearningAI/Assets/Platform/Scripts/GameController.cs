e9ca7611a9fdeac4292a4e12696e51e52d3d5af6
taey1 <ybrgus@gmail.com>
2020-06-05 17:02:33 +0200

    Added scene swap and texts

---
+++ ReinforcementLearningAI/Assets/Platform/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (SceneManager.GetActiveScene() != SceneManager.GetSceneByBuildIndex(1))
+            {
+                SceneManager.LoadScene(1);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (SceneManager.GetActiveScene() != SceneManager.GetSceneByBuildIndex(0))
+            {
+                SceneManager.LoadScene(0);
+            }
+
+        }
+    }
+}
